3c9fa76c192d38a99ecefad453ebac0c6d8857a5
liang bo <kxo@hotmail.com>
2016-12-03 19:06:08 +0800

    finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts
+#  validates name,presence: true,length: { maximum: 10 }    # Replace FILL_IN with the right code.
+ # validates email,presence: true,length: { minimum: 5 }
 end
+
